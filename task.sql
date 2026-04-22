cd1be86a29990dff210a860d889c4acd32a5d5f1
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-04-22 14:25:21 +0300

    Add indexes for ShopDB

--- task.sql
+++ task.sql
@@ -1,2 +1,9 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+CREATE INDEX idx_products ON Products (Name); 
+
+CREATE INDEX idx_customer ON Customers (Email); 
+
+CREATE INDEX idx_orders ON Orders (OrderID); 
+
